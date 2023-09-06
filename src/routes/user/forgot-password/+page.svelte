b65dcbcd3caa0c815e6059cfc5e3ee587f8d8fdd
gustavocadev <gustavocadev@gmail.com>
2023-09-05 21:28:05 -0500

    chore: add forgot password page

---
+++ src/routes/user/forgot-password/+page.svelte
@@ -0,0 +1,16 @@
+<form action="" class="flex flex-col gap-2">
+	<div>
+		<h1 class="text-4xl">Login</h1>
+	</div>
+
+	<div class="flex flex-col gap-1">
+		<label for="email">Email</label>
+		<input type="email" name="email" id="email" class="input" />
+	</div>
+
+	<button class="btn variant-filled-primary" type="submit"> Reset Password </button>
+
+	<p>
+		or <a href="/login" class="font-bold hover:underline">Login</a>
+	</p>
+</form>
