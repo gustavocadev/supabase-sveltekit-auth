548010ef5e7bb9a0aaceaf1722db4405a1fc65aa
gustavocadev <gustavocadev@gmail.com>
2023-09-05 21:28:26 -0500

    chore: change welcome msg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <div class="container mx-auto flex flex-col gap-4">
-	<h1 class="text-4xl">Page</h1>
+	<h1 class="text-4xl">Dashboard Home</h1>
 	<p class="text-4xl">You're Authenticated!</p>
 	<form action="?/signout" use:enhance method="post">
 		<button class="btn variant-filled-error">Sign out</button>
