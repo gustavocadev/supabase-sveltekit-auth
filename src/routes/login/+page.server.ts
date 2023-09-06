c48a1a1dc37c9a7d0dd0f3b845cf3b6ac3406255
gustavocadev <gustavocadev@gmail.com>
2023-09-05 21:32:01 -0500

    chore: add login action

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,6 +1,8 @@
 // src/routes/+page.server.ts
-import { redirect } from '@sveltejs/kit';
+import { redirect, type Actions, fail } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { superValidate } from 'sveltekit-superforms/server';
+import { loginSchema } from '$lib/schemas/user/loginSchema';
 
 export const load: PageServerLoad = async ({ url, locals: { getSession } }) => {
 	const session = await getSession();
@@ -12,3 +14,17 @@ export const load: PageServerLoad = async ({ url, locals: { getSession } }) => {
 
 	return { url: url.origin };
 };
+
+export const actions = {
+	login: async ({ request }) => {
+		const form = await superValidate(request, loginSchema);
+
+		// Convenient validation check:
+		if (!form.valid) {
+			// Again, always return { form } and things will just work.
+			return fail(400, { form });
+		}
+
+		return { form };
+	}
+} satisfies Actions;
