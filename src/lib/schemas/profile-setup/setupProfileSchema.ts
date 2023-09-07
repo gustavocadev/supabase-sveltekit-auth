62443add5c98b0365537fa4567723e769cd12d6e
gustavocadev <gustavocadev@gmail.com>
2023-09-06 20:45:19 -0500

    chore: add setup profile schema

---
+++ src/lib/schemas/profile-setup/setupProfileSchema.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+export const setupProfileSchema = z.object({
+	// avatarUrl: z.string().url(),
+	// firstName: z.string().min(2).max(40),
+	// lastName: z.string().min(2).max(40),
+	countryCode: z.string().min(2).max(3),
+	phoneNumber: z.string().min(6).max(40),
+	codeToVerify: z.string().length(6)
+});
