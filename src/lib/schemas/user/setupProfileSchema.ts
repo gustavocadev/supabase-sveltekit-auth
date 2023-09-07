96dbda984f3605318a6f25be6a48732a07988be0
gustavocadev <gustavocadev@gmail.com>
2023-09-06 20:45:05 -0500

    chore: change schema path

--- src/lib/schemas/user/setupProfileSchema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const setupProfileSchema = z.object({
-	email: z.string().email(),
-	password: z.string().min(6).max(100),
-	confirmPassword: z.string().min(6).max(100)
-});
