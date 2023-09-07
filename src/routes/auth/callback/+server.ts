cfa8b6cef0b96d3f49d438cbf4dc2d8d4bd4bd86
gustavocadev <gustavocadev@gmail.com>
2023-09-06 20:47:32 -0500

    chore: create session after the email was verified

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -1,7 +1,7 @@
 // src/routes/auth/callback/+server.ts
 import { redirect, type RequestHandler } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async ({ url }) => {
+export const GET: RequestHandler = async ({ url, locals }) => {
 	const code = url.searchParams.get('code');
 
 	// if there is no code, we redirect the user to the login page
@@ -10,8 +10,8 @@ export const GET: RequestHandler = async ({ url }) => {
 	}
 
 	// exchangeCodeForSession is a helper function that going to help us to exchange the code for a session, in other words, it's going to help us to log in the user after the email address has been verified, it gonna set a new cookie with the session data and redirect the user to the home page.
-	// await locals.supabase.auth.exchangeCodeForSession(code);
+	await locals.supabase.auth.exchangeCodeForSession(code);
 
 	// we could instead redirect the user to a cuztomized page for confirmed email address
-	throw redirect(303, `/profile/setup?code=${code}`);
+	throw redirect(303, `/profile/setup`);
 };
