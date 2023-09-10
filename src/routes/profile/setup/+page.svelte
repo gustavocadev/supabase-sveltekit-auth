a456ccadeda54409460d95d06a0920e20907c95b
gustavocadev <gustavocadev@gmail.com>
2023-09-10 08:28:44 -0500

    feat: upload images + show country codes popup

--- src/routes/profile/setup/+page.svelte
+++ src/routes/profile/setup/+page.svelte
@@ -2,14 +2,52 @@
 	import { enhance } from '$app/forms';
 	import { MetaTags } from 'svelte-meta-tags';
 	import { superForm } from 'sveltekit-superforms/client';
-	import { Avatar } from '@skeletonlabs/skeleton';
+	import {
+		Autocomplete,
+		Avatar,
+		type AutocompleteOption,
+		popup,
+		type PopupSettings
+	} from '@skeletonlabs/skeleton';
+	import { invalidateAll } from '$app/navigation';
 
 	export let data;
 
+	let countryCode = '';
+	let phoneNumber = '';
+
 	const { form, errors } = superForm(data.form);
 
-	const handleUpdateProfilePhoto = () => {
-		console.log('update profile photo');
+	const handleUpdateProfilePhoto = async (e: Event) => {
+		const target = e.target as HTMLInputElement;
+		const avatar = target.files![0];
+
+		const avatarId = crypto.randomUUID();
+		const avatarExt = avatar.name.split('.').pop();
+		const userId = data.session?.user.id;
+
+		// upload img to supabase storage
+		await data.supabase.storage
+			.from('avatars')
+			.upload(`${userId}/${avatarId}.${avatarExt}`, avatar);
+
+		await invalidateAll();
+	};
+
+	const countryOptions: AutocompleteOption[] = data.countryCodes.map((country) => ({
+		label: `${country.icon} ${country.countryName}`,
+		value: country.countryCode,
+		keywords: [country.countryName, country.countryCode]
+	}));
+
+	function onPopupCountryCodeSelection(event: CustomEvent<AutocompleteOption>): void {
+		countryCode = event.detail.label;
+	}
+
+	let popupSettings: PopupSettings = {
+		event: 'focus-click',
+		target: 'popupAutocomplete',
+		placement: 'bottom'
 	};
 </script>
 
@@ -20,42 +58,71 @@
 
 	<div class="flex flex-col gap-4">
 		<figure class="flex gap-4 justify-center">
-			<Avatar initials="JD" background="bg-primary-500" />
+			<Avatar initials="JD" background="bg-primary-500" src={data.avatar?.signedUrl} width="w-60" />
 		</figure>
-		<button class="btn variant-filled-secondary" type="button" on:click={handleUpdateProfilePhoto}
-			>Upload Image</button
-		>
+		<input type="hidden" name="avatarUrl" />
+		<div class="flex justify-center">
+			<input type="file" name="avatar" on:change={handleUpdateProfilePhoto} />
+		</div>
 	</div>
 
 	<div class="flex flex-col gap-1">
 		<label for="firstName">First Name</label>
-		<input type="text" name="firstName" id="firstName" class="input" bind:value={$form.email} />
+		<input type="text" name="firstName" id="firstName" class="input" />
 	</div>
 
 	<div class="flex flex-col gap-1">
 		<label for="lastName">Last Name</label>
-		<input type="text" name="lastName" id="lastName" class="input" bind:value={$form.password} />
+		<input type="text" name="lastName" id="lastName" class="input" />
 	</div>
 
 	<div class="flex flex-col gap-1">
 		<label for="">Mobile Number</label>
 
-		<div class="flex gap-4">
-			<input type="text" name="countryCode" class="input" />
-			<input type="text" name="phone" class="input" bind:value={$form.confirmPassword} />
-		</div>
+		<form action="?/sendOPTCode" class="flex flex-col gap-4" use:enhance method="post">
+			<div class="flex gap-4">
+				<div class="w-2/12">
+					<input
+						class="input autocomplete"
+						type="search"
+						name="autocomplete-search"
+						bind:value={countryCode}
+						placeholder="Country Code"
+						use:popup={popupSettings}
+					/>
+					<div
+						data-popup="popupAutocomplete"
+						class="card w-full max-w-xs max-h-60 p-4 overflow-y-auto"
+					>
+						<Autocomplete
+							bind:input={countryCode}
+							options={countryOptions}
+							on:selection={onPopupCountryCodeSelection}
+						/>
+					</div>
+				</div>
+
+				<div class="w-10/12">
+					<input
+						type="text"
+						name="phoneNumber"
+						class="input"
+						bind:value={phoneNumber}
+						placeholder="Phone number"
+					/>
+				</div>
+			</div>
+			<button type="submit" class="input p-2 variant-filled-tertiary">Enviar Codigo</button>
+		</form>
 	</div>
 
 	<div class="flex flex-col gap-1">
 		<label for="verifyCode">Verify Code</label>
-		<input
-			type="text"
-			name="codeToVerify"
-			id="verifyCode"
-			class="input"
-			bind:value={$form.password}
-		/>
+		<input type="text" name="codeToVerify" id="verifyCode" class="input" />
 	</div>
 
-	<a class="btn variant-filled-primary" type="submit" href="/">Go to my Dashboard</a>
+	<input type="hidden" bind:value={countryCode} name="countryCode" />
+	<input type="hidden" bind:value={phoneNumber} name="phoneNumber" />
+
+	<button class="btn variant-filled-primary" type="submit">Go to my Dashboard</button>
 </form>
