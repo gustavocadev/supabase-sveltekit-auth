aeb3806d8da0b835d56e4eefeb3c1d8248d98c27
gustavocadev <gustavocadev@gmail.com>
2023-09-10 08:26:39 -0500

    chore: change id type from serial to uuid

--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -1,13 +1,13 @@
-import { boolean, pgTable, serial, text, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { boolean, pgTable, text, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
 
-export const profileTable = pgTable('profile', {
-	id: serial('id').primaryKey(),
+export const profiles = pgTable('profiles', {
+	id: uuid('id').primaryKey(),
 	first_name: varchar('first_name', { length: 100 }),
 	last_name: varchar('last_name', { length: 100 }),
 	phone_number: varchar('phone_number', { length: 100 }),
 	country_code: varchar('country_code', { length: 10 }),
 	avatar_url: text('avatar_url'),
-	phone_validate: boolean('phone_validate').notNull().default(false),
+	phone_validate: boolean('phone_validate').default(false),
 
 	created_at: timestamp('created_at').notNull().defaultNow(),
 	updated_at: timestamp('updated_at').notNull().defaultNow()
