ba5cabd01295aed785e4ac455b11830bc1520bf8
gustavocadev <gustavocadev@gmail.com>
2023-09-10 14:00:25 -0500

    chore: add toast + placeholders

--- src/routes/profile/setup/+page.svelte
+++ src/routes/profile/setup/+page.svelte
@@ -10,6 +10,7 @@
 		type PopupSettings
 	} from '@skeletonlabs/skeleton';
 	import { invalidateAll } from '$app/navigation';
+	import toast from 'svelte-french-toast';
 
 	export let data;
 
@@ -26,23 +27,34 @@
 		const avatarExt = avatar.name.split('.').pop();
 		const userId = data.session?.user.id;
 
+		// delete previous avatar
+		const avatarsBucket = await data.supabase.storage.from('avatars').list(userId);
+		if (avatarsBucket.data) {
+			const latestFile = avatarsBucket.data?.at(0);
+
+			console.log({ latestFile });
+
+			await data.supabase.storage.from('avatars').remove([`${userId}/${latestFile?.name}`]);
+		}
+
 		// upload img to supabase storage
 		await data.supabase.storage
 			.from('avatars')
 			.upload(`${userId}/${avatarId}.${avatarExt}`, avatar);
 
+		// Load all the loaders again
 		await invalidateAll();
 	};
 
 	const countryOptions: AutocompleteOption[] = data.countryCodes.map((country) => ({
-		label: `${country.icon} ${country.countryName}`,
+		label: `${country.icon} ${country.countryCode}`,
 		value: country.countryCode,
 		keywords: [country.countryName, country.countryCode]
 	}));
 
-	function onPopupCountryCodeSelection(event: CustomEvent<AutocompleteOption>): void {
-		countryCode = event.detail.label;
-	}
+	// function onPopupCountryCodeSelection(event: CustomEvent<AutocompleteOption>): void {
+	// 	countryCode = event.detail.label;
+	// }
 
 	let popupSettings: PopupSettings = {
 		event: 'focus-click',
@@ -60,7 +72,7 @@
 		<figure class="flex gap-4 justify-center">
 			<Avatar initials="JD" background="bg-primary-500" src={data.avatar?.signedUrl} width="w-60" />
 		</figure>
-		<input type="hidden" name="avatarUrl" />
+
 		<div class="flex justify-center">
 			<input type="file" name="avatar" on:change={handleUpdateProfilePhoto} />
 		</div>
@@ -68,24 +80,35 @@
 
 	<div class="flex flex-col gap-1">
 		<label for="firstName">First Name</label>
-		<input type="text" name="firstName" id="firstName" class="input" />
+		<input type="text" name="firstName" id="firstName" class="input" placeholder="Eg. Ryan" />
 	</div>
 
 	<div class="flex flex-col gap-1">
 		<label for="lastName">Last Name</label>
-		<input type="text" name="lastName" id="lastName" class="input" />
+		<input type="text" name="lastName" id="lastName" class="input" placeholder="Eg. Carniato" />
 	</div>
 
 	<div class="flex flex-col gap-1">
-		<label for="">Mobile Number</label>
-
-		<form action="?/sendOPTCode" class="flex flex-col gap-4" use:enhance method="post">
+		<form
+			action="?/sendOPTCode"
+			class="flex flex-col gap-4"
+			use:enhance={() => {
+				return ({ result }) => {
+					if (result.type === 'success') {
+						toast.success('Code sent to your phone number');
+					}
+				};
+			}}
+			method="post"
+		>
 			<div class="flex gap-4">
-				<div class="w-2/12">
+				<div class="w-2/12 flex flex-col gap-2">
+					<label for="">Country Code</label>
 					<input
 						class="input autocomplete"
 						type="search"
-						name="autocomplete-search"
+						name="countryCode"
+						autocomplete="off"
 						bind:value={countryCode}
 						placeholder="Country Code"
 						use:popup={popupSettings}
@@ -94,19 +117,17 @@
 						data-popup="popupAutocomplete"
 						class="card w-full max-w-xs max-h-60 p-4 overflow-y-auto"
 					>
-						<Autocomplete
-							bind:input={countryCode}
-							options={countryOptions}
-							on:selection={onPopupCountryCodeSelection}
-						/>
+						<Autocomplete bind:input={countryCode} options={countryOptions} />
 					</div>
 				</div>
 
-				<div class="w-10/12">
+				<div class="w-10/12 flex flex-col gap-2">
+					<label for="">Mobile Number</label>
 					<input
 						type="text"
 						name="phoneNumber"
 						class="input"
+						autocomplete="off"
 						bind:value={phoneNumber}
 						placeholder="Phone number"
 					/>
@@ -118,11 +139,12 @@
 
 	<div class="flex flex-col gap-1">
 		<label for="verifyCode">Verify Code</label>
-		<input type="text" name="codeToVerify" id="verifyCode" class="input" />
+		<input type="text" name="codeToVerify" id="verifyCode" class="input" placeholder="Eg. 123456" />
 	</div>
 
 	<input type="hidden" bind:value={countryCode} name="countryCode" />
 	<input type="hidden" bind:value={phoneNumber} name="phoneNumber" />
+	<input type="hidden" value={data.avatar?.signedUrl} name="avatarUrl" />
 
 	<button class="btn variant-filled-primary" type="submit">Go to my Dashboard</button>
 </form>
