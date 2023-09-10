94b94512cb304d808030d665673152c9e4ca410c
gustavocadev <gustavocadev@gmail.com>
2023-09-10 13:59:20 -0500

    style: uncomment the keys

--- src/lib/schemas/profile-setup/setupProfileSchema.ts
+++ src/lib/schemas/profile-setup/setupProfileSchema.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 
 export const setupProfileSchema = z.object({
-	// avatarUrl: z.string().url(),
-	// firstName: z.string().min(2).max(40),
-	// lastName: z.string().min(2).max(40),
+	avatarUrl: z.string().url().optional(),
+	firstName: z.string().min(2).max(40),
+	lastName: z.string().min(2).max(40),
 	countryCode: z.string().min(2).max(3),
 	phoneNumber: z.string().min(6).max(40),
 	codeToVerify: z.string().length(6)
