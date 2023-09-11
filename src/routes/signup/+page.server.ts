4d54c13ff68ed1be843b007cb6db2013d01a3d75
gustavocadev <gustavocadev@gmail.com>
2023-09-10 22:34:08 -0500

    refactor: add a message if there's a sign up error

--- src/routes/signup/+page.server.ts
+++ src/routes/signup/+page.server.ts
@@ -1,15 +1,14 @@
 import { signupSchema } from '$lib/schemas/user/signupSchema.js';
+import type { Message } from '$lib/types/Message.js';
 import { redirect, type Actions } from '@sveltejs/kit';
 import { fail } from '@sveltejs/kit';
-import { superValidate } from 'sveltekit-superforms/server';
+import { message, superValidate } from 'sveltekit-superforms/server';
 
 export const load = async ({ locals }) => {
 	const session = await locals.getSession();
 
 	// if the user is already logged in return them to the account page
-	if (session) {
-		throw redirect(303, '/');
-	}
+	if (session) throw redirect(303, '/');
 
 	return {
 		form: superValidate(signupSchema)
@@ -18,7 +17,7 @@ export const load = async ({ locals }) => {
 
 export const actions = {
 	signup: async ({ request, locals, url }) => {
-		const form = await superValidate(request, signupSchema);
+		const form = await superValidate<typeof signupSchema, Message>(request, signupSchema);
 
 		if (!form.valid) {
 			return fail(400, {
@@ -26,7 +25,7 @@ export const actions = {
 			});
 		}
 
-		await locals.supabase.auth.signUp({
+		const { error: signUpError } = await locals.supabase.auth.signUp({
 			email: form.data.email,
 			password: form.data.password,
 			options: {
@@ -34,6 +33,23 @@ export const actions = {
 			}
 		});
 
-		return { form };
+		if (signUpError) {
+			// set error for email and password fields
+			return message(
+				form,
+				{
+					type: 'error',
+					text: 'Make sure your email and password are correct'
+				},
+				{
+					status: 400
+				}
+			);
+		}
+
+		return message(form, {
+			type: 'success',
+			text: 'Please check your email to confirm your account.'
+		});
 	}
 } satisfies Actions;
