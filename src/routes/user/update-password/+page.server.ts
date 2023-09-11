96244bf532888f0339d650e72e791f7103ecadb5
gustavocadev <gustavocadev@gmail.com>
2023-09-10 22:37:27 -0500

    chore: load validations and a an action to update the password

---
+++ src/routes/user/update-password/+page.server.ts
@@ -0,0 +1,38 @@
+import { updatePasswordSchema } from '$lib/schemas/user/updatePasswordSchema';
+import { redirect, type Actions, fail } from '@sveltejs/kit';
+import { message, superValidate } from 'sveltekit-superforms/server';
+
+export const load = async ({ locals }) => {
+	const session = await locals.getSession();
+	if (!session) throw redirect(303, '/login');
+
+	const form = await superValidate(updatePasswordSchema);
+	return {
+		form
+	};
+};
+
+export const actions = {
+	updatePassword: async ({ request, locals }) => {
+		const form = await superValidate(request, updatePasswordSchema);
+		if (!form.valid) return fail(400, { form });
+
+		const { data, error } = await locals.supabase.auth.updateUser({
+			password: form.data.password
+		});
+
+		if (error) {
+			return message(
+				form,
+				{
+					type: 'error',
+					text: 'Something went wrong. Please try again.'
+				},
+				{ status: 400 }
+			);
+		}
+
+		console.log(data);
+		throw redirect(303, '/');
+	}
+} satisfies Actions;
