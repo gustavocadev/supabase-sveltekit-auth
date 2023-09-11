2fea96d9af093e55f2a193311108d2655dc29784
gustavocadev <gustavocadev@gmail.com>
2023-09-10 22:23:24 -0500

    refactor: put redirect in one line

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,9 +4,7 @@ export const load = async ({ locals }) => {
 	const session = await locals.getSession();
 
 	// if the user is already logged in return them to the account page
-	if (!session) {
-		throw redirect(303, '/login');
-	}
+	if (!session) throw redirect(303, '/login');
 
 	return {};
 };
