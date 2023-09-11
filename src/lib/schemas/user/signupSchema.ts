8efc6a65c44668c48d755376d468a1d08c459555
gustavocadev <gustavocadev@gmail.com>
2023-09-10 22:34:26 -0500

    chore: add refine to confirm password

--- src/lib/schemas/user/signupSchema.ts
+++ src/lib/schemas/user/signupSchema.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod';
 
-export const signupSchema = z.object({
-	email: z.string().email(),
-	password: z.string().min(6).max(100),
-	confirmPassword: z.string().min(6).max(100)
-});
+export const signupSchema = z
+	.object({
+		email: z.string().email().trim(),
+		password: z.string().min(6).max(100).trim(),
+		confirmPassword: z.string().min(6).max(100).trim()
+	})
+	.refine((data) => data.password === data.confirmPassword, "Passwords don't match");
