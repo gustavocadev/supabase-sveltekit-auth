a76397f6799edf3428d5c4e47165bded8b17eab7
gustavocadev <gustavocadev@gmail.com>
2023-09-10 22:24:30 -0500

    chore: return form + one message

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,36 +1,42 @@
-// src/routes/+page.server.ts
 import { redirect, type Actions, fail } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { superValidate } from 'sveltekit-superforms/server';
+import { message, superValidate } from 'sveltekit-superforms/server';
 import { loginSchema } from '$lib/schemas/user/loginSchema';
+import type { Message } from '$lib/types/Message';
 
-export const load: PageServerLoad = async ({ url, locals: { getSession } }) => {
-	const session = await getSession();
+export const load: PageServerLoad = async ({ url, locals }) => {
+	const session = await locals.getSession();
 
 	// if the user is already logged in return them to the account page
 	if (session) throw redirect(303, '/');
 
-	return { url: url.origin };
+	const form = superValidate(loginSchema);
+	return { url: url.origin, form };
 };
 
 export const actions = {
 	login: async ({ request, locals }) => {
-		const form = await superValidate(request, loginSchema);
+		const form = await superValidate<typeof loginSchema, Message>(request, loginSchema);
 
 		// Convenient validation check:
 		if (!form.valid) {
-			// Again, always return { form } and things will just work.
 			return fail(400, { form });
 		}
 
-		const { data, error } = await locals.supabase.auth.signInWithPassword({
+		const { error: signInError } = await locals.supabase.auth.signInWithPassword({
 			email: form.data.email,
 			password: form.data.password
 		});
 
-		if (error) {
-			console.log(error);
-			return fail(400, { form });
+		if (signInError) {
+			return message(
+				form,
+				{
+					type: 'error',
+					text: 'Make sure you have entered the correct email and password.'
+				},
+				{ status: 400 }
+			);
 		}
 
 		throw redirect(303, '/');
