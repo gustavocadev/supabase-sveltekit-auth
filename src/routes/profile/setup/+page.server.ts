54c343cff281f105ef8e77d70cf123bbf28b0a20
gustavocadev <gustavocadev@gmail.com>
2023-09-10 22:34:51 -0500

    chore: add multiples forms

--- src/routes/profile/setup/+page.server.ts
+++ src/routes/profile/setup/+page.server.ts
@@ -3,89 +3,110 @@ import {
 	sendOPTCodeToPhoneNumber,
 	verifyOPTCode
 } from '$lib/helpers/sendMessage.js';
-import { sendOPTCodeSchema } from '$lib/schemas/profile-setup/sendOPTCodeSchema.js';
+import { sendOPTCodeSchema } from '$lib/schemas/profile-setup/sendOPTCodeSchema';
 import { setupProfileSchema } from '$lib/schemas/profile-setup/setupProfileSchema';
 import { db } from '$lib/server/db.js';
 import { profiles } from '$lib/server/schema.js';
 import { redirect, type Actions, fail } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
-import { superValidate } from 'sveltekit-superforms/server';
+import { message, superValidate } from 'sveltekit-superforms/server';
+import { error } from '@sveltejs/kit';
+import type { Message } from '$lib/types/Message';
 
 export const load = async ({ locals }) => {
 	const session = await locals.getSession();
 	if (!session) throw redirect(303, '/login');
 
 	const userId = session.user.id;
-	const profile = await db.select().from(profiles).where(eq(profiles.id, userId));
+	const profile = (await db.select().from(profiles).where(eq(profiles.id, userId))).at(0);
+
+	if (!profile) {
+		throw error(404, 'Profile not found');
+	}
 
 	// if the user has already setup their profile, redirect them to the home page
-	if (profile.at(0)?.phone_validate) throw redirect(303, '/');
+	if (profile.phone_validate) throw redirect(303, '/');
 
-	const { data } = await locals.supabase.storage.from(`avatars`).list(userId, {
+	const userAvatars = await locals.supabase.storage.from(`avatars`).list(userId, {
 		sortBy: {
 			column: 'created_at'
 		}
 	});
 
-	const firstImgName = data?.reverse()?.at(0)?.name;
+	if (userAvatars.error) throw error(500, userAvatars.error.message);
+	if (!userAvatars.data) throw error(404, 'No images found');
+
+	const firstImgName = userAvatars.data.reverse().at(0)?.name;
 
 	// get the list of country codes
 	const countryCodes = await getCountryCodes();
 
 	// generate a signed url for the user's avatar
-	const avatarUrl = await locals.supabase.storage
+	const urlSignedAvatars = await locals.supabase.storage
 		.from('avatars')
 		.createSignedUrl(`${userId}/${firstImgName}`, 60);
 
+	if (urlSignedAvatars.error) throw error(500, urlSignedAvatars.error.message);
+	if (!urlSignedAvatars.data) throw error(404, 'No images found');
+
+	const setupProfileForm = await superValidate(setupProfileSchema);
+	const sendOPTCodeForm = await superValidate(sendOPTCodeSchema);
 	return {
-		form: superValidate(setupProfileSchema),
+		setupProfileForm,
+		sendOPTCodeForm,
 		countryCodes,
-		avatar: avatarUrl.data
+		avatar: urlSignedAvatars.data
 	};
 };
 
 export const actions = {
 	sendOPTCode: async ({ request }) => {
-		const form = await superValidate(request, sendOPTCodeSchema);
+		const sendOPTCodeForm = await superValidate<typeof sendOPTCodeSchema, Message>(
+			request,
+			sendOPTCodeSchema
+		);
 
-		if (!form.valid) {
-			console.log(form.data);
+		if (!sendOPTCodeForm.valid) {
 			return fail(400, {
-				form
+				sendOPTCodeForm
 			});
 		}
 
-		const { countryCode, phoneNumber } = form.data;
+		const { countryCode, phoneNumber } = sendOPTCodeForm.data;
 
 		// Send SMS
-		await sendOPTCodeToPhoneNumber(`${countryCode}${phoneNumber}`);
+		try {
+			await sendOPTCodeToPhoneNumber(`${countryCode}${phoneNumber}`);
 
-		return {
-			form
-		};
+			return message(sendOPTCodeForm, {
+				type: 'success',
+				text: 'Code sent successfully'
+			});
+		} catch (error) {
+			return message(
+				sendOPTCodeForm,
+				{
+					type: 'error',
+					text: 'Something went wrong. Please try again later.'
+				},
+				{ status: 500 }
+			);
+		}
 	},
 	completeProfile: async ({ request }) => {
-		const form = await superValidate(request, setupProfileSchema);
+		const setupProfileForm = await superValidate(request, setupProfileSchema);
 
-		if (!form.valid) {
-			console.log(form.data);
+		if (!setupProfileForm.valid) {
 			return fail(400, {
-				form
+				setupProfileForm
 			});
 		}
-		const { countryCode, phoneNumber, codeToVerify, firstName, avatarUrl, lastName } = form.data;
+		const { countryCode, phoneNumber, codeToVerify, firstName, avatarUrl, lastName } =
+			setupProfileForm.data;
 
 		const verification_check = await verifyOPTCode(`${countryCode}${phoneNumber}`, codeToVerify);
 
 		if (verification_check.status === 'approved') {
-			console.log({
-				first_name: firstName,
-				last_name: lastName,
-				avatar_url: avatarUrl,
-				phone_validate: true,
-				country_code: countryCode,
-				phone_number: phoneNumber
-			});
 			// Save
 			await db.update(profiles).set({
 				first_name: firstName,
